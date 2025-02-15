2ee686345d6b19cac304cade35e7049c83d493bf
ThomasWilliamson4 <williamson-4@hotmail.com>
2025-02-15 13:13:43 +1030

    Draws a grid

--- maze.py
+++ maze.py
@@ -1,4 +1,43 @@
 from window import Line, Point, Window
+import time
+
+class Maze():
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1 = x1
+        self.y1 =y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        
+        self._create_cells()
+        
+    def _create_cells(self):
+        self._cells = [[Cell(self.win) for _ in range(self.num_rows)] for _ in range(self.num_cols)]
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
+                self._draw_cell(i, j)
+
+
+    def _draw_cell(self, i, j):
+        pos_x = (i*self.cell_size_x) + self.x1
+        pos_y = (j*self.cell_size_y) + self.y1
+        self._cells[i][j].draw(pos_x, pos_y, pos_x+ self.cell_size_x, pos_y+self.cell_size_y)
+        self._animate()
+    
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
 
 
 class Cell():
