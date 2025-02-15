4494499bd592c5af08a9d811d9966009edd6cc9a
ThomasWilliamson4 <williamson-4@hotmail.com>
2025-02-15 12:25:34 +1030

    draw between cells

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ def main():
     cell_1.has_left_wall = False
     cell_1.draw(50, 50, 100, 100)
 
-    cell_1.has_bottom_wall = False
-    cell_1.draw(100, 100, 150, 150)
+    cell_2 = Cell(win)
+    cell_2.has_bottom_wall = False
+    cell_2.draw(100, 100, 150, 150)
+
+    cell_1.draw_move(cell_2)
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -1,4 +1,4 @@
-from window import Line, Point
+from window import Line, Point, Window
 
 
 class Cell():
@@ -12,6 +12,7 @@ class Cell():
         self._y1 = None
         self._y2 = None
         self._win = window
+    
     def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
@@ -29,3 +30,17 @@ class Cell():
         if self.has_bottom_wall:
             line = Line(Point(x1, y2),Point(x2, y2))
             self._win.draw_line(line, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        from_x = (self._x1 + self._x2)/2
+        from_y = (self._y1 + self._y2)/2
+        to_x = (to_cell._x1 + to_cell._x2)/2
+        to_y = (to_cell._y1 + to_cell._y2)/2
+        line = Line(Point(from_x, from_y), Point(to_x, to_y))
+                
+        if undo == False:
+            self._win.draw_line(line,"red")
+        else:
+            self._win.draw_line(line, "gray")
+        
+        
